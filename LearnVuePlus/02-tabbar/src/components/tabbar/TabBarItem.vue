4ee4de2ba9f9725a804b4430f7cc1d990a25e7b7
yuanfang000205 <2296787770@qq.com>
2021-07-25 22:24:09 +0800

    tabbar组件的简单使用

---
+++ LearnVuePlus/02-tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem"
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    text-align: center;
+    height: 49px;
+    flex: 1;
+  }
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
